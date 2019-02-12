e32b6b87c7bec171ffb08d64a7ef50b9603c8063
Daniel Thompson <1dptt@queensu.ca>
2019-02-12 15:15:26 -0500

    psuedo code added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,41 @@
-console.log("connected");
+//Variables
+//============================================
+//Global Variables
+var scoreToGet [];
+var  gems = [1,2,3,4];
+var currentScore [];
+var gemValue= [];
+
+//Game Counter Variables
+var winCounter= [];
+var lossCounter= [];
+
+
+
+//Functions
+//============================================
+
+function init(){
+    //generate random score to get between 19-120
+    //generate random values for each gem between 1-12
+    //set win counter and loss counter to 0
+    //set users current score to 0
+    //render everything that needs to be rendered.. using magical jQuery
+}
+
+function reset() {
+    //generate  random score to get between 12-120
+    //generate random values for each gem between 1-12
+    //update win/loss counter
+    //reset users current score to 0
+}
+
+//Main Process
+//============================================
+
+//set listeners for each of the gem buttons 
+//when a gem is clicked add that gems value to the users current score
+//if else if for score matching and exceeding the score to get
+
+
+
